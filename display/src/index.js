c10c6e28ad87fb40a218310b8d44375785fc2b7f
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-12-19 14:25:51 +0100

    🎨 Mise à jour du template tennis pour scortenn

--- display/src/index.js
+++ display/src/index.js
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import ScoringMode from "./Components/ScoringMode";
+import { I18nextProvider } from "react-i18next";
+import LogoMode from "./Components/LogoMode";
 import MediaMode from "./Components/MediaMode";
 import PrematchMode from "./Components/PrematchMode.js";
 import QRMode from "./Components/QRMode.js";
-import config from "./config.js";
-import LogoMode from "./Components/LogoMode";
-import "./main.css"
-import i18n from "./config/i18n/i18n.js";
-import Basketball from "./Components/Sports/Basketball/Basketball.js";
-import Handball from "./Components/Sports/Handball/Handball.js";
-import Volleyball from "./Components/Sports/Volleyball/Volleyball.js";
-import Tennis from "./Components/Sports/Tennis/Tennis.js";
+import ScoringMode from "./Components/ScoringMode";
 import TestPage from "./Components/TestPage.js";
-import modeService from "./service/modeService.js";
-import { I18nextProvider } from "react-i18next";
-const { ipcRenderer } = window.require("electron");
+import i18n from "./config/i18n/i18n.js";
+import "./main.css";
+import Tennis from "./Components/Sports/Tennis/Tennis.js";
+/* const { ipcRenderer } = window.require("electron"); */
 
 const root = document.getElementById("root");
 const appRoot = ReactDOM.createRoot(root);
@@ -28,7 +23,7 @@ const App = () => {
   const [lastMediaMode, setLastMediaMode] = useState(null); // Track last media mode
 
 
-  useEffect(() => {
+/*   useEffect(() => {
     const removeListeners = () => {
       ipcRenderer.removeAllListeners("server-data");
     };
@@ -72,20 +67,21 @@ const App = () => {
     });
 
     return removeListeners;
-  }, [lastMediaMode]); // Include lastMediaMode in the dependencies array
+  }, [lastMediaMode]); */ // Include lastMediaMode in the dependencies array
 
 
   return (
     <>
       <I18nextProvider i18n={i18n}>
-        {mode === "scoring" && <ScoringMode gameState={gameState} />}
+  {/*       {mode === "scoring" && <ScoringMode gameState={gameState} />}
         {mode === "media" && <MediaMode key={mediaKey} mediaState={mediaState} mediaMode={mediaMode} />}
         {mode === "prematch" && <PrematchMode mediaState={mediaState} mediaMode={mediaMode} gameState={gameState} />}
         {mode === "logo" && <LogoMode />}
         {mode === "test" && <TestPage />}
         {mode === "qr" && <QRMode />}
         {mode === "sleep" && <></>}
-        {mode === "" && <div>Waiting for data...</div>}
+        {mode === "" && <div>Waiting for data...</div>} */}
+        <Tennis />
       </I18nextProvider>
     </>
   );
